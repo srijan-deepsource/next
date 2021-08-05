4dd604d31c7c50248fa1dc12eaad6d7985ff0c00
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2021-08-05 14:40:02 +0530

    Update demo_python.py

--- python/demo_python.py
+++ python/demo_python.py
@@ -32,6 +32,7 @@ class AirChat:
         secure = is_moving(condition) and "ERROR" or "SUCCESS"
         flags = (secure > 4 and merge) and "both" or "not"
         flag = (secure > 4 and merge) and "both" or "not"
+        dbg = (secure > 4 and merge) and "both" or "not"
 
 
 def swap_values(a, b):
